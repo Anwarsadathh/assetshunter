05b23ee259c70b54197f9e05918af9ec4fc7ad4d
anwar sadath <anwaranu633@gmail.com>
2024-12-02 22:27:18 +0530

    first commitsssfglhadsaaasss

--- app.js
+++ app.js
@@ -191,8 +191,10 @@ app.set("view engine", "hbs");
 
 // Middleware setup
 app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+// Increase payload size limits
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/uploads", express.static("uploads"));
